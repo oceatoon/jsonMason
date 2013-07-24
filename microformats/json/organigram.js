8260778cee7694861b1684739b6acd34341f9845
Tibor Katelbach <oceatoon@gmail.com>
2013-07-24 14:18:11 +0400

    pixel humain design
    - added hover effect
    - on onclick selected

--- microformats/json/organigram.js
+++ microformats/json/organigram.js
@@ -1,58 +1,6 @@
-var organigramData = {
-	"Nous Trouver" : {
-		"Trello" : {"link":"https://trello.com/board/pixel-humain-echolocal/50a3e15a175358d65a0089ef"},
-		"Blog" : {"link":"http://blog.pixelhumain.com/"},
-		"Twitter" : "",
-		"FaceBook" : {"link":"https://www.facebook.com/groups/pixelhumain/"},
-		"Diigo":{"link":"https://groups.diigo.com/group/pixelhumain"},
-		"Mailing List" : {"link":"https://groups.google.com/forum/?hl=fr&fromgroups=#!forum/pixelhumain"},
-		"Mail" : {"link":"contact@pixelhumain.com"},
-		"Google Community" : "Restraint pour le moment",
-		"Google Drive" : "Restraint pour le moment",
-		"Google Maps" : {}
-	},
-	"Pixels Actifs" : {
-		"Graphiste" : {
-			"Stephanie Lorente":{
-				"Tel":"",
-				"Mail":""
-			},
-			"Jermey Loureau":{
-				"Tel":"",
-				"Mail":""	
-			}
-		},
-		"Developpement":{
-			"Tibor Katelach":{
-				"Tel":"",
-				"Mail":""
-			}
-		},
-		"DRH":{
-			"Sylvain Barbot":{
-				"Tel":"",
-				"Mail":""
-			}
-		},
-		"Bénévoles":{
-			"Kevin Lainé":{
-				"Mail":""
-			}
-		},
-		"Administratif":{
-			"Jerome Gonthier":{
-				"Tel":"",
-				"Mail":""
-			}
-		},
-		"Nous cherchons":[
-			"Assistante Administrative",
-			"Analyste Financier",
-			"Community Manager",
-			"Génie Marketing"
-		]
-	}	
-};
+//TODO : url hashing for Real Simple History and back button feautres
+//TODO : make people generic meaning only one definition location
+//BUG : on 3rd level
 var debug = true;
 function log(){
 	if(debug)
@@ -65,54 +13,119 @@ function dir(){
 Organigram = function(data){
 	
 	var self = this;
-	this.clickIds = [];
+	this.clickIds = {};
+	this.clickIdsBreadCrumb = [];
+	this.openIdsBreadCrumb = [];
 	this.data = data;
+	this.currentlyOpen = null;
+	this.back = null;
 	
-	this.render = function(json,width,lvl,hide)
+	
+	this.render = function(json)
 	{
 		dir(json);
-		var classUl = "";
-		if(hide)
-			classUl = "hidden";
-		
-		var html ='<ul id="lvl'+lvl+'" class="'+classUl+'">';
-		var width = width/_.size(json);
-		var childCount = 0;
-		$.each(json, function(key, val){
-			//html += "<div class='block' style='width:"+width+"%; background-color:grey;'>"+key;
-			
-			classLi = (typeof val == "object" ) ? 'clickable' : '';
-			
-			html += '<li id="child'+lvl+childCount+'" class="'+classLi+'">'+key;
-			html += '</li>';
-			if(typeof val == "object" ){
-				//builds recursivly 
-				html += self.render(val,width,lvl+""+childCount,true);
-				
-				//post building helps bind click events to objects
-				self.clickIds.push({"parent" : "child"+lvl+childCount,
-									"open" : "lvl"+lvl+""+childCount});
+		var html = "";
+		$.each(json, function(key, val)
+		{
+			log("level1", key);
+			//build first level doors
+			html += "<div id='"+key+"' class='txt block topblock'><div>"+val.content.toUpperCase()+"</div></div>";
+			self.clickIds[key] = val.system.events;
+			if(val.open)
+			{
+				log("level2" );
+				//build 2nd level doors
+				html+="<div class='hidden block' id='"+val.open.system.id+"'><ul>";
+				var heightLi = 100/_.size(val.open.list);
+				htmlLvl3 = "";
+				$.each(val.open.list, function(ki, vi)
+				{
+					var id = "";
+					if(vi.system && vi.system.events){
+						self.clickIds[ki] = vi.system.events;
+						id = "id='"+vi.system.id+"'" ;
+					}
+					html+="<li "+id+" style='height:"+heightLi+"%' class='txt'><div>";
+					html+= (vi.link) ? "<a href='"+vi.link+"'>"+ki.toUpperCase()+"</a>" : ki.toUpperCase();
+					html+="</div></li>";
+					//build 3rd level doors
+					log("level2" ,ki,vi);
+					if(vi.open && vi.system)
+					{
+						log("level3" );
+						htmlLvl3 +="<div class='hidden block' id='"+vi.system.id+"List'><ul>";
+						var heightLii = 100/_.size(vi.open.list);
+						$.each(vi.open.list, function(kii, vii)
+						{
+							if(vii.system && vii.system.events){
+								self.clickIds[ki+"_"+kii] = vii.system.events;
+								id = "id='"+ki+"_"+kii+"'" ;
+							}
+							htmlLvl3+="<li "+id+" style='height:"+heightLii+"%' class='txt txt2'><div>";
+							label = (vii.content) ? vii.content.toUpperCase(): kii.toUpperCase() ;
+							label= (vii.link) ? "<a href='"+vii.link+"'>"+label+"</a>" : label;
+							//hack to vertical align
+							htmlLvl3+=label;
+							htmlLvl3+="</div></li>";
+							log("level3" ,kii,vii);
+						});
+						htmlLvl3 += "</div></ul>";
+						
+					}
+				});
+				html += "</div></ul>";
+				html += htmlLvl3;
 			}
-			else
-				html += " " +val;
-			
-			childCount++;
-			//html += "</div>";
 		});
-		html +='</ul>';
-		
-		
 		return html;
 	};
-	this.activeClicks = function(){
-		$.each(self.clickIds, function(i, link){
-			$("#"+link.parent).click(function(){
-				$(".active").removeClass("active");
-				$(this).addClass("active");
-				$("#"+link.open).slideToggle();
-				log("found siblings",$("#"+link.open).siblings().length);
-				//$("#"+link.open).siblings().addClass("hidden");
-			})
+	
+	this.activeElements = function()
+	{
+		//$('#workTypes').insertBefore('#door1');
+		$.each(self.clickIds, function(key, events)
+		{
+			log("activeElements",key);
+			if(events.onclick){
+				var tmpClick = events.onclick;
+				events.onclick = function(source){
+					self.clickIdsBreadCrumb.unshift(key);
+					log("clicked",key,self.clickIdsBreadCrumb);
+					
+					tmpClick(source);
+					
+					//manage genericaly selected css
+					if(!$(this).hasClass('blockSelected') )
+						$(this).addClass('blockSelected');
+					else {
+						$("#"+key+" div").html($(this).data("data-text"));
+						$(this).removeClass('blockSelected');
+					}
+				};
+				$("#"+key).click(events.onclick);
+			}
+			//BUG : marche pas qd bg color set in css
+			if(events.onhover){
+				$("#"+key).mouseover(function(){
+					//$(this).data("data-initColor",$(this).css('backgroundColor'));
+					if($(this).hasClass('blockSelected')){
+						$(this).data("data-text",$("#"+key+" div").html());
+						//$(this).css("background-color","yellow");
+						$("#"+key+" div").html("BACK");
+					}
+					$(this).addClass('blockHovered');
+				});
+				$("#"+key).mouseout(function(){
+					//log("data-initColor",$(this).data("data-initColor"));
+					if($(this).hasClass('blockSelected'))
+						$("#"+key+" div").html($(this).data("data-text"));
+					/* provoque un BUG a la recup de la couleur ???
+					 * else if($(this).data("data-initColor")!="transparent")
+						$(this).css("background-color",$(this).data("data-initColor"));*/
+					
+					$(this).removeClass('blockHovered');
+				});
+			}
 		});
 		
 	}
